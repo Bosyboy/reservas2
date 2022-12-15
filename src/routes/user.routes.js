5fd770e69ae7298de7cb94946a9c06b539dc7b0b
Bosyboy <bosyboy@gmail.com>
2022-12-15 20:41:42 -0300

    necesito dormir

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { isLoggedIn } from "../lib/auth.js";
 import { renderUserProfile } from "../controllers/user.controller.js";
-import { listarReservas } from "../controllers/reservas.controller.js";
+
 
 const router = Router();
 
-router.get("/reservas", isLoggedIn, listarReservas);
+router.get("/", isLoggedIn, renderUserProfile);
 
 
 export default router;
